53e04e7c44fce907ab717ca499faebac56219a50
Hanmo QIN <hqinac@connect.ust.hk>
2021-05-06 01:49:15 +0800

    BSpline(finished)

--- BSplineCurveEvaluator.h
+++ BSplineCurveEvaluator.h
@@ -17,13 +17,18 @@ public:
 		const float& fAniLength,
 		const bool& bWrap) const override;
 private:
+
+	void Wrap(const std::vector<Point>& ptvCtrlPts,
+		std::vector<Point>& ptvEvaluatedCurvePts,
+		float fAniLength, bool bWarp) const;
+
 	void noWrap(const std::vector<Point>& ptvCtrlPts,
 		std::vector<Point>& ptvEvaluatedCurvePts,
-		float fAniLength) const;
+		float fAniLength, bool bWarp) const;
 
 	void evaluateBSpline(int i, const std::vector<Point>& ptvCtrlPts,
 		std::vector<Point>& ptvEvaluatedCurvePts,
-		float fAniLength) const;
+		float fAniLength, bool bWarp) const;
 
 	static Mat4f BSMatrix;
 };
